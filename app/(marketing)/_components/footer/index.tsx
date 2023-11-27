5a99ac5cb885c4413edcaddde95d82c03291cdef
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-11-28 00:21:45 +0400

    feat: footer created for marketing page

---
+++ app/(marketing)/_components/footer/index.tsx
@@ -0,0 +1,22 @@
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
@@ -5,6 +6,7 @@ const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="bg-slate-100 h-full">
       <Navbar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
     </div>
   );
 };
