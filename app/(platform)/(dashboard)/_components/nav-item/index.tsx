ec46145f3eadd3982d55506700ade32fa1dd216c
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-11-28 11:50:07 +0400

    feat: skeleton added for sidebar loading

--- app/(platform)/(dashboard)/_components/nav-item/index.tsx
+++ app/(platform)/(dashboard)/_components/nav-item/index.tsx
@@ -5,6 +5,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { Organization } from "@/types";
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
@@ -89,6 +90,16 @@ const NavItem = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  );
+};
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
   );
 };
 
--- app/(platform)/(dashboard)/_components/sidebar/index.tsx
+++ app/(platform)/(dashboard)/_components/sidebar/index.tsx
@@ -55,7 +55,15 @@ const Sidebar = ({ storageKey = "t-sidebar-state" }: ISidebarProps) => {
   )
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
 
