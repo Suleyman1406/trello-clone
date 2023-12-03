e3a2b7a7fd987ea1f8f027c7f909258708e2fca3
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-12-04 01:13:45 +0400

    feat: audit log feature added to project

---
+++ lib/create-audit-log.ts
@@ -0,0 +1,38 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
+import { db } from "@/lib/db";
+
+interface Props {
+  entityId: string;
+  entityType: ENTITY_TYPE;
+  entityTitle: string;
+  action: ACTION;
+}
+
+export const createAuditLog = async (props: Props) => {
+  try {
+    const { orgId } = auth();
+    const user = await currentUser();
+
+    if (!user || !orgId) {
+      throw new Error("User not found!!");
+    }
+    const { entityId, entityTitle, entityType, action } = props;
+
+    await db.auditLog.create({
+      data: {
+        orgId,
+        entityId,
+        entityType,
+        entityTitle,
+        userId: user.id,
+        action,
+        userImage: user.imageUrl,
+        userName: `${user.firstName} ${user.lastName}`,
+      },
+    });
+  } catch (error) {
+    console.error("[AUDIT_LOG_ERROR]", error);
+  }
+};
